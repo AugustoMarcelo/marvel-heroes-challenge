510bf2925e57352597b7acd0a131bb22d2e26ad8
AugustoMarcelo <mrclgst10@gmail.com>
2020-06-19 09:18:43 -0300

    Removed comments

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -138,17 +138,10 @@ export const CharacterName = styled.Text`
   font-size: 20px;
   font-family: 'gilroy-heavy';
   color: #fff;
-  /* position: absolute;
-  bottom: 15px;
-  left: 15px;
-  width: 90px; */
 `;
 
 export const CharacterAlterEgo = styled.Text`
   font-size: 10px;
   font-family: 'gilroy-medium';
   color: #fff;
-  /* position: absolute;
-  bottom: 60px;
-  left: 15px; */
 `;
